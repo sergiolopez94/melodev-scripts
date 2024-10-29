a5c732d188a8a221c279c2e55f902eb912cd0626
sergiolopez94 <sergiolopez@melodev.com>
2024-10-29 02:33:26 -0400

    removed <script>

--- webflow-to-s3.js
+++ webflow-to-s3.js
@@ -1,5 +1,4 @@
-<script>
   document.addEventListener("DOMContentLoaded", function() {
-    console.log("JavaScript loaded and ready.")
+    console.log("JavaScript loaded and ready.");
   });
-</script>
+
